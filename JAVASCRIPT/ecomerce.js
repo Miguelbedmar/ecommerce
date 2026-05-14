1eec8a619563fff0d73a61aa83e0b121408f1e21
Miguel <miguelbedmards@gmail.com>
2026-05-14 19:52:59 +0200

    Se ha creado  otra funcion para  añadir productos al carro

--- JAVASCRIPT/ecomerce.js
+++ JAVASCRIPT/ecomerce.js
@@ -1,4 +1,4 @@
-
+//DECLARACION DE VARIABLES
 const productos = [
   {
     id: 1,
@@ -49,7 +49,7 @@ const productos = [
     stock: 50
     },
 ];
-
+let carrito= [];
 /**CREO UNA FUNCIÓN PARA RECORRER LA LISTA DE LOS PRODUCTOS UTILIZANDO UN METODO QUE 
  * RECORRA LA LISTA UTILIZANDO EL FOREACH PORQUÉ ? Y  OTRA IMPORTANTE SERÍA PORQUE NO
  *  EL METODO MAP ? PUES ES BASTANTE SIMPLE Y ES QUE NO ME INTERESA QUE  SE GENERE
@@ -75,3 +75,18 @@ caja.appendChild(tarjeta);
 
 }
 recorrerProductos(productos);
+
+/*agregarAlCarrito es una función que busca si el producto 
+*existe en el carrito si no existe añade el objeto y si existe
+*suma +1 a la cantidad de ese objeto.
+*/
+function agregarAlCarrito(id) {
+  const productoCarrito = carrito.find();
+
+  if(productoCarrito){
+
+  }else{
+    const producto = producto.find();
+    carrito.push();
+  }
+}
